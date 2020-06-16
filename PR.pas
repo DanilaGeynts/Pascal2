ab639c43032376b528e9e219e4d3cdc435604a3c
Danila Geynts <danil.geynts.00@mail.ru>
2020-06-17 00:06:57 +0500

    3

--- PR.pas
+++ PR.pas
@@ -1,5 +1,5 @@
 ﻿program ;
-var a,b,c,d:real;
+var a,b,c,d,f:real;
 begin
 writeln('Введите первое число');
 readln(a);
@@ -9,4 +9,6 @@ c:=a+b;
 writeln('Сумма = ',c);
 d:=a*b;
 writeln('Произведение = ',d);
+f:=(a*b)-(a+b);
+writeln('Разность произведения и суммы чисел = ',f);
 end.
